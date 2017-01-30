dcfe0048708d1c8e3b3de641a0d92986a00defeb
anahtreek <keerthana.n_23@yahoo.com>
2017-01-30 05:24:17 +0000

    With test cases

--- js/s+s.js
+++ js/s+s.js
@@ -1,56 +1,79 @@
-const fs = require('fs');
-const readline = require('readline');
-const Stream = require('stream');
-let instream = fs.createReadStream('../inputdata/FoodFacts.csv');
-let outstream = new Stream();
-let rl = readline.createInterface(instream, outstream);
-let ss = [];
-let c;
-c = ['Netherlands', 'Canada', 'UK/USA', 'Australia', 'France', 'Germany', 'Spain', 'South Africa'];
-let sugar = [0, 0, 0, 0, 0, 0, 0, 0];
-let salt = [0, 0, 0, 0, 0, 0, 0, 0];
-function cntrindex(cntr) {
-  let ind = -1;
-  if(cntr) {
-    for(let i = 0; i < c.length; i = i + 1) {
-      if(cntr.includes(c[i])) {
-        ind = i;
-      }
-    }
-  }
-  return ind;
-}
-rl.on('line', function(line) {
-  let currentLine = line.split(/,(?=(?:(?:[^"]*"){2})*[^"]*$)/);
+/*  let csvFile = '../inputdata/FoodFacts.csv';
+let jsonFile = '../outputdata/sugarsalt.json';*/
+module.exports = function(csvFile, jsonFile) {
+  let ss = [];
+  let c;
+  c = ['Netherlands', 'Canada', 'UK/USA', 'Australia',
+   'France', 'Germany', 'Spain', 'South Africa'];
+  let sugar = [0, 0, 0, 0, 0, 0, 0, 0];
+  let salt = [0, 0, 0, 0, 0, 0, 0, 0];
+  let linenumber = 0;
+  let countryIndex;
+  let sugarIndex;
+  let saltIndex;
   let index;
-  if(currentLine[33]) {
-    if(currentLine[33].includes(',')) {
-      let x = currentLine[33].split(',');
-      for(let j=0; j < x.length; j = j + 1) {
-        index = cntrindex(x[j]);
-        if(index !== -1) {
-          sugar[index] = sugar[index] + Number(currentLine[102]);
-          salt[index] = salt[index] + Number(currentLine[116]);
+  let ret;
+  function cntrindex(cntr) {
+    let ind = -1;
+    if(cntr) {
+      for(let i = 0; i < c.length; i = i + 1) {
+        if(cntr.includes(c[i])) {
+          ind = i;
         }
       }
     }
-    else {
-      index = cntrindex(currentLine[33]);
-      if(index !== -1) {
-        sugar[index] = sugar[index] + Number(currentLine[102]);
-        salt[index] = salt[index] + Number(currentLine[116]);
+    return ind;
+  }
+  if(!csvFile || !jsonFile) {
+    ret = 'Enter parameters';
+  }
+  else {
+    const fs = require('fs');
+    const readline = require('readline');
+    const Stream = require('stream');
+    let instream = fs.createReadStream(csvFile);
+    let outstream = new Stream();
+    let rl = readline.createInterface(instream, outstream);
+    rl.on('line', function(line) {
+      let currentLine = line.split(/,(?=(?:(?:[^"]*"){2})*[^"]*$)/);
+      if(linenumber === 0) {
+        let title = currentLine;
+        countryIndex = title.indexOf('countries_en');
+        sugarIndex = title.indexOf('sugars_100g');
+        saltIndex = title.indexOf('salt_100g');
+        linenumber = 1;
       }
+      if(currentLine[countryIndex] && currentLine[countryIndex].includes(',')) {
+        let x = currentLine[countryIndex].split(',');
+        for(let j = 0; j < x.length; j = j + 1) {
+          index = cntrindex(x[j]);
+          if(index !== -1) {
+            sugar[index] = sugar[index] + Number(currentLine[sugarIndex]);
+            salt[index] = salt[index] + Number(currentLine[saltIndex]);
+          }
+        }
+      }
+      else if(currentLine[countryIndex]) {
+        index = cntrindex(currentLine[countryIndex]);
+        if(index !== -1) {
+          sugar[index] = sugar[index] + Number(currentLine[sugarIndex]);
+          salt[index] = salt[index] + Number(currentLine[saltIndex]);
+        }
+      }
+    });
+    rl.on('close', function() {
+      for(let i = 0; i < c.length; i = i + 1) {
+        let obj = {};
+        obj.country = c[i];
+        obj.sugar = sugar[i];
+        obj.salt = salt[i];
+        ss.push(obj);
+      }
+      fs.writeFileSync(jsonFile, JSON.stringify(ss));
+    });
+    if(ss) {
+      ret = 'JSON written successfully';
     }
   }
-  
-});
-rl.on('close', function() {
-  for(let i = 0; i < c.length; i = i + 1) {
-    let obj = {};
-    obj.country = c[i];
-    obj.sugar = sugar[i];
-    obj.salt = salt[i];
-    ss.push(obj);
-  }
-  fs.writeFileSync('../outputdata/sugarsalt.json', JSON.stringify(ss));
-});
+  return ret;
+};
